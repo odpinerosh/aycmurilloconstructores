6ca86020f896bf3f6294e0c0599102b43b4bef0e
Stackbit Code Editor <projects@stackbit.com>
2021-11-20 02:06:48 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -32,7 +32,7 @@ export default class ContactSection extends React.Component {
                             <label htmlFor="subject">Motivo del contacto</label>
                             <div className="form-select-wrap">
                                 <select name="subject" id="subject">
-                                    <option value="">Please select</option>
+                                    <option value="">Seleccione</option>
                                     <option value="Business Inquiries">Business Inquiries</option>
                                     <option value="Sponsorship">Partnerships</option>
                                     <option value="Other">Other</option>
